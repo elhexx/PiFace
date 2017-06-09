d8a00dbe804721e3ed895d8a58da97aa7470bc42
elhexx <elhexx@gmail.com>
2017-06-09 17:28:13 +0100

    .

--- main.py
+++ main.py
@@ -1,17 +1,16 @@
 import cv2
-import numpy as np 
+import numpy as np
 import os
-import serial
 
-ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
+# import serial
+
+# ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
 
 cascadePath = "haarcascade_frontalface_default.xml"
 faceCascade = cv2.CascadeClassifier(cascadePath)
-recognizer=cv2.face.createLBPHFaceRecognizer()
+recognizer = cv2.face.createLBPHFaceRecognizer()
 
-
-
-#Loading each image and cropping the face preparing the dataset
+# Loading each image and cropping the face preparing the dataset
 
 # for filename in os.listdir('Dataset'):
 # 	im=cv2.imread('Dataset/'+filename,0)
@@ -19,53 +18,49 @@ recognizer=cv2.face.createLBPHFaceRecognizer()
 # 	faces = faceCascade.detectMultiScale(im, 1.3, 5)
 # 	for (x,y,w,h) in faces:
 # 		cv2.imwrite('Dataset/'+filename,im[y:y+h, x:x+w])
-	
 
 
-images=[]
-labels=[]
+
+images = []
+labels = []
 for filename in os.listdir('Dataset'):
-	im=cv2.imread('Dataset/'+filename,0)
-	images.append(im)
-	labels.append(int(filename.split('.')[0][0]))
-	#print filename
+    im = cv2.imread('Dataset/' + filename, 0)
+    images.append(im)
+    labels.append(int(filename.split('.')[0][0]))
+# print filename
 
 
-names_file=open('labels.txt')
-names=names_file.read().split('\n')
+names_file = open('labels.txt')
+names = names_file.read().split('\n')
 
-
-recognizer.train(images,np.array(labels))
+recognizer.train(images, np.array(labels))
 print 'Training Done . . . '
 
 font = cv2.FONT_HERSHEY_SIMPLEX
-cap=cv2.VideoCapture(1)
-lastRes=''
-count=0
 
-while(1):
-	_,frame=cap.read()
-	gray=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
-	faces = faceCascade.detectMultiScale(gray, 1.3, 5)
-	count+=1
+lastRes = ''
+count = 0
 
-	for (x,y,w,h) in faces:
-		cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
-		if count>20:
-			res=names[recognizer.predict(gray[y:y+h, x:x+w])-1]
-			if res!=lastRes :
-				lastRes=res
-				print lastRes
-				ser.write(lastRes)
-			count=0
-		break
+while (1):
+    frame = cv2.imread("brad.jpg")
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = faceCascade.detectMultiScale(gray, 1.3, 5)
+    count += 1
 
-	
-	
-	cv2.imshow('frame',frame)
-	k = 0xFF & cv2.waitKey(10)
-	if k == 27:
-		break
-cap.release()
-ser.close()
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
+        if count > 20:
+            res = names[recognizer.predict(gray[y:y + h, x:x + w])[0] - 1]
+            if res != lastRes:
+                lastRes = res
+                print "face detected : " + lastRes
+            # ser.write(lastRes)
+            count = 0
+        break
+
+    cv2.imshow('frame', frame)
+    k = 0xFF & cv2.waitKey(10)
+    if k == 27:
+        break
+
 cv2.destroyAllWindows()
